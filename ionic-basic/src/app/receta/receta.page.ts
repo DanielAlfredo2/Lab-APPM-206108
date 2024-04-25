f2b3f127d013e5dd6616dedbf25319fbf96ab228
DanielAlfredo2 <daspnueve@gmail.com>
2024-04-24 20:46:32 -0600

    Practica 10
    Edicion destino, Borrado destino en firebase, refactorizacion de rutas para usar el guard con can activate en rutas main,

--- ionic-basic/src/app/receta/receta.page.ts
+++ ionic-basic/src/app/receta/receta.page.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { RecetaService } from '../service/receta.service';
 import { Receta } from '../interface/receta';
+import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-receta',
-  templateUrl: './receta.page.html'
-})
+  @Component({
+    selector: 'app-receta',
+    templateUrl: './receta.page.html'
+
+  })
 export class RecetaPage implements OnInit {
 
   recetas: Receta[] = [];
 
-  constructor(private recetaService: RecetaService) { }
+  constructor(    
+    private recetaService: RecetaService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
     this.recetas = this.recetaService.getRecetas();
+  }
+
+  irDetalleReceta(id: number){
+    this.router.navigate([`main/detalle-receta/${id}`]);
   }
 
 }
